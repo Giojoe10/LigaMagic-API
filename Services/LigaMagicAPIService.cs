0b70b6f029eb84ae3d4c454baab74fe8e083662c
Giojoe10 <60120182+Giojoe10@users.noreply.github.com>
2022-08-15 11:41:25 -0300

    removed useless code

--- Services/LigaMagicAPIService.cs
+++ Services/LigaMagicAPIService.cs
@@ -8,17 +8,7 @@ namespace LigaMagicAPI.Services;
 
     
 public class LigaMagicAPIService{
-
-    static List<Card> Cards{get;}
-    static int nextId = 1;
-
-    static LigaMagicAPIService(){
-        Cards = new List<Card>{};
-    }
-
-
-    public static List<Card> GetCards()=>Cards;
-    
+   
     public static Card Get(string name){
         return Card.CardFactory(name);
     }
