f6fcf6660d6efb9ce781e96b83eae6661a2c3843
Giojoe10 <60120182+Giojoe10@users.noreply.github.com>
2022-08-15 12:07:43 -0300

    cleaning up code

--- Models/Card.cs
+++ Models/Card.cs
@@ -13,15 +13,9 @@ public class Card{
     public string? Name{get;private set;}
     public Dictionary<string, double>? Precos{get; private set;}
 
-    [OnDeserialized()]
-    internal void OnDecerializedMethod(StreamingContext context){
-        Precos = GetPrices();
-    }
-
     [JsonConstructor]
     public Card(string name){
         Name = name;
-        
         Precos = GetPrices();
     }
     
@@ -29,7 +23,6 @@ public class Card{
         Card card = CardFromName(name).Result;
         return card;
     }
-
 
     public async static Task<Card> CardFromName(string name) {
         string? parsedName = ParseName(name);
@@ -41,7 +34,6 @@ public class Card{
         return new Card("Colossal Dreadmaw");
     }
 
-
     public void PrintPrecos(){
         if(Precos is null){
             return;
@@ -52,6 +44,7 @@ public class Card{
                 Console.WriteLine(preco.Key+": R$"+preco.Value);
         }
     }
+    
     public double GetMenorPreco(){
         if(Precos is null){
             return -1;
@@ -66,10 +59,6 @@ public class Card{
             Name = "Colossal Dreadmaw";
         }
         var parsedName = ParseName(Name);
-        //parsedName = Uri.EscapeDataString(parsedName);
-        //Console.WriteLine(Name + ": " + parsedName);
-        //parsedName = parsedName.Replace(" ", "%20");
-        //parsedName = parsedName.Replace("/", "%2F");
 
         var stream = GetWebPage($"https://www.ligamagic.com.br/?view=cards/card&card={parsedName}").Result;
         if(stream is null){
@@ -93,7 +82,6 @@ public class Card{
             setName = HttpUtility.HtmlDecode(setName);
             var preco = set.Value?.Value<double>("precoMenor");
             
-            // var preco = setObj.Value<double>("precomenor");
             if(preco!=null && preco!=0){
                 prices[setName] = preco.Value;
             }
@@ -107,9 +95,7 @@ public class Card{
     public static async Task<Stream> GetWebPage(string url){
         client.DefaultRequestHeaders.Accept.Clear();
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json") { CharSet = "utf-8" });
-        //client.DefaultRequestHeaders.Add(new NameValueHeaderValue(""))
         var streamTask = await client.GetStreamAsync(url);
-        //string page = await stringTask;
         return streamTask;
 
     }
@@ -118,25 +104,4 @@ public class Card{
     {
         return name?.Split('/')[0].Trim().Replace(" ", "%20").Replace("/", "%2F");
     }
-
-
-
-    // public string GetWebPage(string url){
-    //     WebRequest request = WebRequest.Create(url);
-    //     request.Method="GET";
-    //     HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-    //     using(Stream stream = response.GetResponseStream()){
-    //         StreamReader reader = new StreamReader(stream);
-    //         string? result = reader.ReadToEnd();
-    //         reader.Close();
-    //         return result;
-    //     }
-    // }
-    
-    // private async Task<string>? ScryfallName(string name){
-    //     var parsedName = name.Replace(" ", "+");
-    //     var url = $"https://api.scryfall.com/cards/named?fuzzy={parsedName}";
-    //     var result = GetWebPage(url);
-    //     return JObject.Parse(result).Value<string>("name");
-    // }
 }
